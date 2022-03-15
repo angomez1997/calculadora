a2b35fd6dad6a904b8150537da32dfa95e33b3bb
Angie Gomez <angiegomez0706@gmail.com>
2022-03-15 11:03:57 -0500

    sin j.s

---
+++ script.js
@@ -0,0 +1,96 @@
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const datos = e =>{
+    let datosPersonas = JSON.parse(localStorage.getItem("datosPersonas")) || [];
+    datosPersonas.push({
+        form: document.querySelector("form").value,
+        sexo : document.querySelector("sexo").value,
+        edad : document.querySelector("edad").value,
+        peso : document.querySelector("peso").value,
+        altura : document.querySelector("altura").value,
+        calcular : document.getElementById("#resultado").value,
+        range: document.getElementById("#valor").value,
+       
+
+    });
+
+    
+    localStorage.setItem("datospersonas", JSON.stringify(datosPersonas));
+    dispData();
+    e.preventdefault();
+}
+function iniciar(){
+    boton= document.getElementById("#boton").value,
+    boton.addEvenlistener("click", clickBtncalcular);
+}
+function clickBtncalcular(){
+
+
+}*/
+
+            
+
+
+        
+
+
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
